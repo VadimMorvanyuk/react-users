8c5ba6192deafba43c52a62cba2bf27b770072ee
Vadim Morvanyuk <vadim.morvanyuk@gmail.com>
2023-06-15 16:52:45 +0300

    add hook and structure

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import Card from "../UI/Card";
+import './UserForm.css';
+
 const UserForm = () => {
-  return <div>User Form</div>;
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+
+  const nameHandler = (event) => {
+    setName(value => value = event.target.value)
+  }
+  const ageHandler = (event) => {
+    setAge(value => value = event.target.value);
+  }
+
+  return (
+    <Card>
+      <form>
+        <label htmlFor="name">User Name</label>
+        <input id='name' type="text" onChange={nameHandler} value={name}/>
+        <label htmlFor="age">Age (Years)</label>
+        <input id='age' type="number" onChange={ageHandler} value={age} />
+      </form>
+    </Card>
+    )
 };
 
 export default UserForm;
